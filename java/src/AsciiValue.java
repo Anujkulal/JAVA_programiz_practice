e2d54efdd5bf6f10fb591949533d403ef98c501e
anujkulal <anujkulal333@gmail.com>
2024-06-13 06:54:20 +0530

    commit

---
+++ java/src/AsciiValue.java
@@ -0,0 +1,9 @@
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch = 'd';
+        // int ascii = ch;
+        // //or
+        int ascii = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);   
+    }
+}
